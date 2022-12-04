7b8111d335d58feedafb119573c5fc33b190eacb
Daniel Serezane <serezane@protonmail.com>
2022-12-04 14:00:28 -0300

    cohen: mais ajustes

--- cohen/johnny.py
+++ cohen/johnny.py
@@ -41,7 +41,7 @@ class Cohen_Sutherland:
         res = self.cohen_sutherland(x1, y1, x2, y2)
         print(res)
         if res:
-            draw.line(res[:4], fill=(0, 255, 0))
+            draw.line(res, fill=(0, 255, 0))
         '''draw = ImageDraw.Draw(self.image)
         res = self.cohen_sutherland((x1, y1), (x2, y2))
         draw.line((x1, y1, x2, y2), fill=(255, 0, 0))
@@ -71,48 +71,41 @@ class Cohen_Sutherland:
         # que ambos estejam dentro da janela de corte.
         # O algoritmo vai, no máximo, precisar ajustar 4 pontos.
         while True:
-            # Quando ambos estiverem na janela de corte
-            # (ou se naturalmente já estiverem)
+            # Quando ambos estiverem na janela de corte após os ajustes,
+            # ou se naturalmente já estiverem.
             if cod1 == 0 and cod2 == 0:
-                return (x1, y1, x2, y2, True)
+                return (x1, y1, x2, y2)
             # Se ambos estiverem fora da janela de corte
             elif (cod1 & cod2) != 0:
                 return False
-
             # Se ainda precisarem de ajustes, usamos as fórmulas
             x, y = None, None
             # Tratamos apenas os pontos que precisam de ajuste
             # (conseguimos saber se precisa ou não pelo código)
-            if cod1 != 0:
-                codeOut = cod1
-            else:
-                codeOut = cod2
-            
+            cod_tr = cod1 if cod1 != 0 else cod2
             # Fazendo direto ao invés de calcular m para evitar divisão por zero
             # Cima
-            if codeOut & TOP:
+            if cod_tr & TOP:
                 x = x1 + (x2 - x1) * (self.yb - y1) / (y2 - y1)
                 y = self.yb
             # Baixo
-            elif codeOut & BOTTOM:
+            elif cod_tr & BOTTOM:
                 x = x1 + (x2 - x1) * (self.yt - y1) / (y2 - y1)
                 y = self.yt
             # Direita
-            elif codeOut & RIGHT:
+            elif cod_tr & RIGHT:
                 x = self.xl
                 y = y1 + (y2 - y1) * (self.xl - x1) / (x2 - x1)   
             # Esquerda 
-            elif codeOut & LEFT:
+            elif cod_tr & LEFT:
                 x = self.xr
                 y = y1 + (y2 - y1) * (self.xr - x1) / (x2 - x1)
-            
             # Atualiza o código do ponto que precisava de ajuste
-            if codeOut == cod1:
+            if cod_tr == cod1:
                 x1 = x
                 y1 = y
                 cod1 = self.point_classify(x1, y1)
                 continue
-
             x2 = x
             y2 = y
             cod2 = self.point_classify(x2, y2)
