315e291f597b1a4ff181a5485df27f8fa0f965db
Daniel Serezane <serezane@protonmail.com>
2022-12-04 01:50:36 -0300

    cohen implementado e testado
    falta refatorar

--- cohen/johnny.py
+++ cohen/johnny.py
@@ -7,6 +7,19 @@ RIGHT = 2
 BOTTOM = 4
 TOP = 8
 
+def getPoint(x,y, xMax, yMax, xMin, yMin):
+    code = INSIDE
+    if x < xMin:
+        code |= LEFT
+    elif x > xMax:
+        code |= RIGHT
+    if y < yMin:
+        code |= BOTTOM
+    elif y > yMax:
+        code |= TOP
+    
+    return code
+
 class Cohen_Sutherland:
     def __init__(self):
         self.image = Image.new('RGB', (500, 500), (13,117,172))
@@ -34,89 +47,95 @@ class Cohen_Sutherland:
         print("Drawing line from ({}, {}) to ({}, {})".format(x1, y1, x2, y2))
         draw = ImageDraw.Draw(self.image)
         draw.line((x1, y1, x2, y2), fill=(255, 0, 0))
-        print(self.cohen_sutherland((x1, y1), (x2, y2)))
+        res = self.cohen_sutherland(x1, y1, x2, y2)
+        print(res)
+        if res:
+            draw.line(res[:4], fill=(0, 255, 0))
         '''draw = ImageDraw.Draw(self.image)
         res = self.cohen_sutherland((x1, y1), (x2, y2))
         draw.line((x1, y1, x2, y2), fill=(255, 0, 0))
         xa, ya, xb, yb = res
         draw.line((xa, ya, xb,yb), fill=(255, 255, 255))'''
 
-    def point_classify(self, x, y):
-        print(x, y)
-        c = 0b0000
-
-        if x < self.xl:
-            c |= 1
-        elif x > self.xr:
-            c |= 2
-
-        if y < self.yb:
-            c |= 4
-        elif y > self.yt:
-            c |= 8
-
-        return c
-
     # O algoritmo de Cohen-Sutherland, propriamente dito.
     # Retorna o (x1, y1, x2, y2) da reta cortada com a janela de corte estabelecida,
     # ou False caso a reta esteja inteiramente fora da janela de corte.
-    def cohen_sutherland(self, p1, p2):
-        #print('xl {} yb {} xr {} yt {}'.format(self.xl, self.yb, self.xr, self.yt))
-        # Classificando os pontos
-        cod = [self.point_classify(*p1), self.point_classify(*p2)]
-        if cod[0] == 0 and cod[1] == 0:
-            return p1 + p2
-        elif cod[0] & cod[1] != 0:
+    def cohen_sutherland(self, x1, y1, x2, y2):
+        # compute region codes for P1, P2
+        xMax, yMax, xMin, yMin = self.xl, self.yb, self.xr, self.yt
+        codeA = getPoint(x1, y1, xMax, yMax, xMin, yMin)
+        codeB = getPoint(x2, y2, xMax, yMax, xMin, yMin)
+        isInside = False
+        
+        while True:
+            # if both endpoints lie within rectangle
+            if codeA == 0 and codeB == 0:
+                isInside = True
+                break
+            
+            # if both endpoints are outside rectangle
+            elif (codeA & codeB) != 0:
+                break
+            
+            # Some segment lies within the rectangle
+            else:
+                # line needs clipping because at least
+                # one of the points is outside the rectangle
+                x = 1.0
+                y = 1.0
+                # we find which of the points is outside
+                if codeA != 0:
+                    codeOut = codeA
+                else:
+                    codeOut = codeB
+                
+                # now we find the intersection point using
+                # some formulas
+                if codeOut & TOP:
+                    # point is above the clip rectangle
+                    x = x1 + (x2 - x1) * (yMax - y1) / (y2 - y1)
+                    y = yMax
+                
+                elif codeOut & BOTTOM:
+                    # point is below the clip rectangle
+                    x = x1 + (x2 - x1) * (yMin - y1) / (y2 - y1)
+                    y = yMin
+                    
+                elif codeOut & RIGHT:
+                    # point is to the right of the clip rectangle
+                    y = y1 + (y2 - y1) * (xMax - x1) / (x2 - x1)
+                    x = xMax
+                    
+                elif codeOut & LEFT:
+                    # point is to the left of the clip rectangle
+                    y = y1 + (y2 - y1) * (xMin - x1) / (x2 - x1)
+                    x = xMin
+                    
+                # now we replace point outside rectangle by
+                # intersection point
+                if codeOut == codeA:
+                    x1 = x
+                    y1 = y
+                    codeA = getPoint(x1, y1, xMax, yMax, xMin, yMin)
+                else:
+                    x2 = x
+                    y2 = y
+                    codeB = getPoint(x2, y2, xMax, yMax, xMin, yMin)
+
+        if isInside:
+            return (x1, y1, x2, y2, True)
+        else:
             return False
-        codeA, codeB = cod
-        # line needs clipping because at least
-        # one of the points is outside the rectangle
-        x = 1.0
-        y = 1.0
-        # we find which of the points is outside
-        if codeA != 0:
-            codeOut = codeA
-        else:
-            codeOut = codeB
-            
-        # now we find the intersection point using
-        # some formulas
-        if codeOut & TOP:
-            # point is above the clip rectangle
-            x = x1 + (x2 - x1) * (self.ymax - y1) / (y2 - y1)
-            y = self.ymax
-            
-        elif codeOut & BOTTOM:
-            # point is below the clip rectangle
-            x = x1 + (x2 - x1) * (self.ymin - y1) / (y2 - y1)
-            y = self.ymin
-                
-        elif codeOut & RIGHT:
-            # point is to the right of the clip rectangle
-            y = y1 + (y2 - y1) * (self.xmax - x1) / (x2 - x1)
-            x = self.xmax
-                
-        elif codeOut & LEFT:
-            # point is to the left of the clip rectangle
-            y = y1 + (y2 - y1) * (self.xmin - x1) / (x2 - x1)
-            x = self.xmin
-                
-        # now we replace point outside rectangle by
-        # intersection point
-        if codeOut == codeA:
-            x1 = x
-            y1 = y
-            codeA = self.point_classify(x1, y1)
-        else:
-            x2 = x
-            y2 = y
-            codeB = self.point_classify(x2, y2)
         
     def show(self):
         self.image.show()
 
 c = Cohen_Sutherland()
 c.set_window(100, 100, 400, 400)
-#c.draw_line(300, 450, 400, 20)
+c.draw_line(300, 450, 400, 20)
 c.draw_line(110, 30, 50, 400)
+c.draw_line(200, 450, 250, 60)
+c.draw_line(240, 440, 110, 80)
+c.draw_line(60, 320, 440, 240)
+c.draw_line(440, 380, 90, 340)
 c.show()
